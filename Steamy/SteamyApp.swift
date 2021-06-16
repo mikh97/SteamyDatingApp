eacf03af4d94846201d10eeb21b2ec8dc96df4ca
Neev <neevalkumar@gmail.com>
2021-06-16 06:25:09 -0700

    Initial Commit

---
+++ Steamy/SteamyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SteamyApp.swift
+//  Steamy
+//
+//  Created by Neeval Kumar on 6/16/21.
+//
+
+import SwiftUI
+
+@main
+struct SteamyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
