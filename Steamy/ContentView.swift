d8a343e8596529c21dc1b14a360e727e857ad940
Neev <neevalkumar@gmail.com>
2021-06-19 00:06:58 -0700

    Worked on views for the 2 start screens

--- Steamy/ContentView.swift
+++ Steamy/ContentView.swift
@@ -7,20 +7,36 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
         
-        Text("Hello")
-            .padding()
-    }
+        ZStack {
+           Color(red: 0.94, green: 0.30, blue: 0.22)
+            .ignoresSafeArea()
+            
+            VStack(alignment: .center) {
+            Image("SteamyIcon")
+            
+                Text("Steamy")
+                    .font(.system(size: 43, weight: .semibold, design: .rounded ))
+                    .foregroundColor(Color.white)
+                    
+                    
+            }
+        
+        }
     
         
+}
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+    
+    
 }
+
+
---
+++ Steamy/Sign_OR_Log_UIView.swift
@@ -0,0 +1,69 @@
+//
+//  Sign_OR_Log_UIView.swift
+//  Steamy
+//
+//  Created by Neeval Kumar on 6/18/21.
+//
+
+import SwiftUI
+
+struct Sign_OR_Log_UIView: View {
+    var body: some View {
+        ZStack {
+           Color(red: 0.94, green: 0.30, blue: 0.22)
+            .ignoresSafeArea()
+            
+            VStack(alignment: .center) {
+            
+                Text("Find your Perfect Match Today")
+                    .font(.system(size: 43, weight: .semibold, design: .rounded ))
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.leading)
+                
+                VStack{
+                    Spacer(minLength: 50).frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height:120, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                    Button(action: {
+                            print("Login Up Pressed")
+                    }) {
+                        Text("LOGIN")
+                            .frame(width: 200, height: 0, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .font(.system(size: 20, weight: .semibold, design: .rounded))
+                            .padding()
+                            .background(Color.yellow)
+                            .foregroundColor(.white)
+                            .border(Color.yellow, width:5)
+                            .cornerRadius(40)
+                            
+                    }
+                    
+                    Spacer(minLength: 50).frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height:10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    
+                    Button(action: {
+                            print("Sign Up Pressed")
+                    }) {
+                        Text("SIGN UP")
+                            .frame(width: 200, height: 0, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .font(.system(size: 20, weight: .semibold, design: .rounded))
+                            .padding()
+                            .background(Color.white)
+                            .foregroundColor(.black)
+                            .border(Color.white, width:5)
+                            .cornerRadius(40)
+                            
+                    }
+                    
+                           
+                    
+                }
+            }
+        
+        }
+    }
+}
+
+struct Sign_OR_Log_UIView_Previews: PreviewProvider {
+    static var previews: some View {
+        Sign_OR_Log_UIView()
+    }
+}
