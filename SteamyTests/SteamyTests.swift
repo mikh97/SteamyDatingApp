670435eec08966ef41ee5d4214852bec26b90767
Mikhail Sumawan <mikh1738@gmail.com>
2021-06-30 19:10:37 +0700

    Steamy Unit Test Files

--- SteamyTests/SteamyTests.swift
+++ SteamyTests/SteamyTests.swift
@@ -8,8 +8,8 @@
 import XCTest
 
 class SteamyTests: XCTestCase {
-
-    override func setUpWithError() throws {
+    
+   override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
@@ -28,5 +28,4 @@ class SteamyTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
